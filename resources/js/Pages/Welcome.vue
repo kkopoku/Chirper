f1e6ad64be4aa26ce212e2703434dd70c4e7b260
kkopoku <kk.opoku@outlook.com>
2022-11-14 07:50:11 +0000

    minor change

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -18,32 +18,35 @@ defineProps({
     <div>
       <img style='width: 100%; height: 100%;' v-bind:src="'assets/images/background01.jpg'">
     </div>
-    <div class="grid items-center justify-center">
+    <div class="grid items-center justify-center align-middle">
+
+      <div class="text-4xl">
+        Welcome To Chirper
+      </div>
       
-      <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-        class="text-sm text-gray-700 dark:text-gray-500 underline">
+      <Link v-if="$page.props.auth.user" :href="route('dashboard')">
           <button class="px-4 py-1 text-lg text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-            Dashboard
+            Go Back To Dashboard
           </button>
       </Link>
 
       <template v-else>
         <div v-if="canLogin" class="hidden sm:block">
 
-          <div class="grid grid-cols-1 grid-rows-2 justify-items-center gap-7">
+          <div class="grid grid-cols-1 grid-rows-2 gap-7">
 
           <div>
-            <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">
+            <Link :href="route('login')">
               <button class="px-4 py-1 text-lg text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                login
+                Login Hereeeeeeeeeee !
               </button>
             </Link>
           </div>
 
           <div>
-            <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
+            <Link v-if="canRegister" :href="route('register')">
               <button class="px-4 py-1 text-lg text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                Register
+                Click Me To Register !
               </button>
             </Link>
           </div>
